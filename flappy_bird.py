2def2d164a69e06637df30b49df7a02ab9a566ef
S. Dhyanesh <dhyan130709@gmail.com>
2023-05-16 00:12:31 +0530

    Add bird flapping

--- flappy_bird.py
+++ flappy_bird.py
@@ -17,14 +17,19 @@ pygame.display.set_caption("Flappy Bird By Dhyanesh")
 bird = Bird(display)
 bird.start()
 
+BIRD_FLAP = pygame.USEREVENT
+pygame.time.set_timer(BIRD_FLAP, 100)
+
 while True:
     display.fill((0, 0, 0))
-    for event in pygame.event.get ():
-        if event.type == pygame.QUIT: #To exit program
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
         if (event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE) or event.type == pygame.MOUSEBUTTONDOWN:
             bird.jump()
+        if event.type == BIRD_FLAP:
+            bird.flap()
     bird.render()
     pygame.display.update()
     clock.tick(FPS)
