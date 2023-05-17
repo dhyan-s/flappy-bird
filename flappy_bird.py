d104b0034bb23f1dfaf9f91d1b90732291debf29
S. Dhyanesh <dhyan130709@gmail.com>
2023-05-17 13:58:37 +0530

    Add sounds

--- flappy_bird.py
+++ flappy_bird.py
@@ -16,6 +16,10 @@ display = pygame.display.set_mode((SCREENWIDTH , SCREENHEIGHT))
 clock = pygame.time.Clock()
 pygame.display.set_caption("Flappy Bird By Dhyanesh")
 
+crash_sound = pygame.mixer.Sound("assets/sounds/crash.mp3")
+point_sound = pygame.mixer.Sound("assets/sounds/point.ogg")
+jump_sound = pygame.mixer.Sound("assets/sounds/jump.ogg")
+
 bird = Bird(display)
 bird.start()
 
@@ -33,8 +37,10 @@ pygame.time.set_timer(ADDPIPE, 900)
 def collision():
     pipe_manager.stop()
     bird.stop()
+    crash_sound.play()
     
 bird_pipe_interaction_manager.add_collision_callback(collision)
+bird_pipe_interaction_manager.add_pass_through_callback(lambda: point_sound.play())
 
 while True:
     display.fill((0, 0, 0))
@@ -44,6 +50,7 @@ while True:
             sys.exit()
         if (event.type  == pygame.KEYDOWN and event.key == pygame.K_SPACE) or event.type == pygame.MOUSEBUTTONDOWN:
             bird.jump()
+            jump_sound.play()
         if event.type == BIRD_FLAP:
             bird.flap()
         if event.type == ADDPIPE:
