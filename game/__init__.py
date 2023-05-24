0c2681c22192abc6481b9db9e7b954acb4d83b57
S. Dhyanesh <dhyan130709@gmail.com>
2023-05-24 19:31:00 +0530

    (Refractor) Update __all__ in game/__init__.py

--- game/__init__.py
+++ game/__init__.py
@@ -2,3 +2,5 @@ from .game import Game
 from .home_screen import HomeScreen
 from .display_handler import DisplayHandler
 from .score import Score
+
+__all__ = ["Game", "HomeScreen", "DisplayHandler", "Score"]
